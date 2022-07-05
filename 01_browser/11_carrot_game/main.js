4a35384aa32f632a21f4f06feacd1e935c0f1332
simpler1204 <suwoni7335@naver.com>
2022-07-05 15:43:57 +0800

    carrot_game

---
+++ 01_browser/11_carrot_game/main.js
@@ -0,0 +1,84 @@
+'use strict'
+
+const CARROT_SIZE = 80;
+const CARROT_COUNT = 5;
+const BUG_COUNT = 5;
+const field = document.querySelector('.game__field');
+const fieldRect = field.getBoundingClientRect();
+const gameBtn = document.querySelector('.game__button');
+const gameTimer = document.querySelector('.game__timer');
+const gameScore = document.querySelector('.game__score');
+
+let started = false;
+let score = 0;
+let timer = undefined
+
+gameBtn.addEventListener('click', ()=>{
+   if(started){
+    stopGame();
+   }else{
+    startGame();
+   }
+   started = !started;
+})
+
+function startGame(){
+    initGame();
+    showStopButton();
+    showTimerAndScore();
+    startGameTimer();
+}
+
+function stopGame(){
+
+}
+
+function showStopButton(){
+    const icon = gameBtn.querySelector('.fa-play');
+    icon.classList.add('fa-stop');
+    icon.classList.remove('fa-play');
+}
+
+function showTimerAndScore(){
+    gameTimer.style.visibility = 'visible';
+    gameScore.style.visibility = 'visible';
+}
+
+
+function initGame(){
+    field.innerHTML = '';
+    gameScore.innerText = CARROT_COUNT;
+    // 벌레와 당근을 생성한뒤 field에 추가해줌
+    console.log(fieldRect);
+    addItem('carrot', CARROT_COUNT, 'img/carrot.png');
+    addItem('but', BUG_COUNT, 'img/bug.png')
+}
+
+function getRandomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  
+
+function addItem(className, count, imgPath){
+
+    const x1 = 0;
+    const y1 = 0;
+    const x2 = fieldRect.width - CARROT_SIZE;
+    const y2 = fieldRect.height - CARROT_SIZE;
+
+    for(let i = 0; i<count; i++){
+        
+        const item = document.createElement('img');
+        item.setAttribute('src', imgPath);
+        item.setAttribute('class', className);
+        item.style.position = 'absolute';
+        const x = getRandomNumber(x1, x2);
+        const y = getRandomNumber(y1, y2);         
+        item.style.left = `${x}px`;
+        item.style.top = `${y}px`;
+        
+        field.appendChild(item);
+    }
+
+    
+}
